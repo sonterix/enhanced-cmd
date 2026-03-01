7991662f4648815a86dbd67f2ccc85e9dc389d91
Sonterixo <sonterixo@gmail.com>
2026-03-01 01:18:57 -0700

    Add layout setting and display map

--- Config.lua
+++ Config.lua
@@ -6,8 +6,10 @@ ns.DEFAULTS          = {
     maxPerRow     = 6,
     growDirection = "DOWN",
     align         = "CENTER",
+    layout        = "STATIC",
 }
 
 -- Display text for UI labels
 ns.DIRECTION_DISPLAY = { DOWN = "Down", UP = "Up" }
 ns.ALIGN_DISPLAY     = { LEFT = "Left", CENTER = "Center", RIGHT = "Right" }
+ns.LAYOUT_DISPLAY    = { STATIC = "Static", DYNAMIC = "Dynamic" }
